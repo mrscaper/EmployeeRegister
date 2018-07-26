77d393c54327920c2fc91f3d3152a7e462cc2dc3
Botond Seres <seres_botond@hotmail.com>
2018-07-26 20:04:06 +0200

    Employer validation

--- EmployeeRegister/Models/Extensions/Employer.cs
+++ EmployeeRegister/Models/Extensions/Employer.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
 
 namespace EmployeeRegister.Models
 {
@@ -11,6 +13,8 @@ namespace EmployeeRegister.Models
         internal sealed class EmployerMetadata
         {
             [Required] [MaxLength(255)] public string Name;
+            [Display(Name="General Contractor")] public int EmployerId;
+            [Display(Name = "General Contractor")] public Employer GeneralContractor;
         }
     }
 }
