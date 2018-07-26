7da6d0edd7cb566087402d9a6fc6348bc7a211a7
Botond Seres <seres_botond@hotmail.com>
2018-07-26 20:01:57 +0200

    Database setup scripts

---
+++ EmployeeRegister/Scripts/Sql/dbo.Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]         INT         NOT NULL IDENTITY,
+    [Deleted]    BIT         NULL,
+    [Name]       NCHAR (255) NOT NULL,
+    [Email]      NCHAR (255) NULL,
+    [EmployerId] INT         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Employee_ToEmployer] FOREIGN KEY ([EmployerId]) REFERENCES [dbo].[Employer] ([Id])
+);
+
---
+++ EmployeeRegister/Scripts/Sql/dbo.Employer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employer] (
+    [Id]         INT         NOT NULL IDENTITY,
+    [Name]       NCHAR (255) NOT NULL,
+    [Deleted]    BIT         NULL,
+    [EmployerId] INT         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Name] ASC),
+    CONSTRAINT [FK_Employer_ToEmployer] FOREIGN KEY ([EmployerId]) REFERENCES [dbo].[Employer] ([Id])
+);
+
